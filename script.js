9b2b545a8b845cb8a66a9fd99461363780563a98
dinellipaulo <pgsdinelli@gmail.com>
2025-12-04 19:14:33 -0400

    optimized js code

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 function main() {
     const frame = document.querySelector('.frame');
     const resetBtn = document.querySelector('.resetBtn');
+    const gridNumber = 16;
 
-    resetBtn.addEventListener("click", () => {
-        const cells = document.querySelectorAll('.cell').forEach(cell => {
-            cell.style.backgroundColor = 'white'
-        });
-    })
-
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < gridNumber; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
 
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < gridNumber; j++) {
             const cell = document.createElement('div');
             cell.classList.add('cell');
             row.appendChild(cell);
         }
-        
+
         frame.appendChild(row);
-        
-        const cells = document.querySelectorAll('.cell').forEach(cell => {
+
+        const cells = document.querySelectorAll('.cell');
+
+        cells.forEach(cell => {
             cell.addEventListener("mouseover", () => {
                 cell.style.backgroundColor = 'black';
-            })
+            });
+        })
+
+        resetBtn.addEventListener("click", () => {
+            cells.forEach(cell => {
+                cell.style.backgroundColor = 'white'
+            });
         })
     }
 }
