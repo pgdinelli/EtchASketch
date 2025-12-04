02efc5fc7deeb18a77e3990f88f9c2ccb6d9fc66
dinellipaulo <pgsdinelli@gmail.com>
2025-12-04 19:36:44 -0400

    Resize grid button - needs fix

--- script.js
+++ script.js
@@ -1,34 +1,54 @@
 function main() {
     const frame = document.querySelector('.frame');
     const resetBtn = document.querySelector('.resetBtn');
-    const gridNumber = 16;
+    const resizeBtn = document.querySelector('.resizeBtn');
+    let gridNumber = 16;
+    createGrid(gridNumber);
 
-    for (let i = 0; i < gridNumber; i++) {
-        const row = document.createElement('div');
-        row.classList.add('row');
-
-        for (let j = 0; j < gridNumber; j++) {
-            const cell = document.createElement('div');
-            cell.classList.add('cell');
-            row.appendChild(cell);
+    resizeBtn.addEventListener("click", () => {
+        const userInput = prompt('Enter the size of the grid:');
+        if (Number(userInput)) {
+            gridNumber = userInput;
+            createGrid(gridNumber);
+        } else {
+            console.log('Not a valid value! Please insert a number.');
         }
+    })
 
-        frame.appendChild(row);
-
-        const cells = document.querySelectorAll('.cell');
-
-        cells.forEach(cell => {
-            cell.addEventListener("mouseover", () => {
-                cell.style.backgroundColor = 'black';
-            });
-        })
-
-        resetBtn.addEventListener("click", () => {
-            cells.forEach(cell => {
-                cell.style.backgroundColor = 'white'
-            });
-        })
+    function removeGrid() {
+        frame.removeChild(row);
     }
+
+    function createGrid(gridNumber) {
+        for (let i = 0; i < gridNumber; i++) {
+            const row = document.createElement('div');
+            row.classList.add('row');
+
+            for (let j = 0; j < gridNumber; j++) {
+                const cell = document.createElement('div');
+                cell.classList.add('cell');
+                row.appendChild(cell);
+            }
+
+            frame.appendChild(row);
+
+            const cells = document.querySelectorAll('.cell');
+
+            cells.forEach(cell => {
+                cell.addEventListener("mouseover", () => {
+                    cell.style.backgroundColor = 'black';
+                });
+            })
+
+            resetBtn.addEventListener("click", () => {
+                cells.forEach(cell => {
+                    cell.style.backgroundColor = 'white'
+                });
+            })
+        }
+    }
+
+
 }
 
 main();
