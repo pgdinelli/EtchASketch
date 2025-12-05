3508550165521cc2d1cf72bb9d493af942c18e65
dinellipaulo <pgsdinelli@gmail.com>
2025-12-04 20:12:53 -0400

    resize grid feature + style improvements

--- script.js
+++ script.js
@@ -3,23 +3,21 @@ function main() {
     const resetBtn = document.querySelector('.resetBtn');
     const resizeBtn = document.querySelector('.resizeBtn');
     let gridNumber = 16;
-    createGrid(gridNumber);
+    generateGrid(gridNumber);
 
     resizeBtn.addEventListener("click", () => {
-        const userInput = prompt('Enter the size of the grid:');
-        if (Number(userInput)) {
-            gridNumber = userInput;
-            createGrid(gridNumber);
+        const userInput = prompt('Enter the size of the grid (between 1 and 100):');
+
+        if (isNaN(userInput) || userInput < 1 || userInput > 100) {
+            alert('Not a valid value! Enter sizes between 1 and 100.');
         } else {
-            console.log('Not a valid value! Please insert a number.');
+            gridNumber = parseInt(userInput);
+            removeGrid();
+            generateGrid(gridNumber);
         }
     })
 
-    function removeGrid() {
-        frame.removeChild(row);
-    }
-
-    function createGrid(gridNumber) {
+    function generateGrid(gridNumber) {
         for (let i = 0; i < gridNumber; i++) {
             const row = document.createElement('div');
             row.classList.add('row');
@@ -31,24 +29,28 @@ function main() {
             }
 
             frame.appendChild(row);
-
-            const cells = document.querySelectorAll('.cell');
-
-            cells.forEach(cell => {
-                cell.addEventListener("mouseover", () => {
-                    cell.style.backgroundColor = 'black';
-                });
-            })
-
-            resetBtn.addEventListener("click", () => {
-                cells.forEach(cell => {
-                    cell.style.backgroundColor = 'white'
-                });
-            })
         }
+
+        const cells = document.querySelectorAll('.cell');
+
+        cells.forEach(cell => {
+            cell.addEventListener("mouseover", () => {
+                cell.style.backgroundColor = 'black';
+            });
+        })
+
+        resetBtn.addEventListener("click", () => {
+            cells.forEach(cell => {
+                cell.style.backgroundColor = 'white'
+            });
+        })
     }
 
-
+    function removeGrid() {
+        while (frame.firstChild) {
+            frame.removeChild(frame.lastChild);
+        }
+    }
 }
 
 main();
